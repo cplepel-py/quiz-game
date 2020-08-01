abe820db0aec65c45f7998041bc03f1d79decd4d
unknown <cbplepel@gmail.com>
2020-08-01 15:31:20 -0500

    Add user login API

--- api/api.js
+++ api/api.js
@@ -2,11 +2,13 @@ const MongoError = require("mongodb").MongoError;
 const {db} = require("./db-utils");
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 router.use(express.json());
 
 const hashRounds = 12;
+const jwtSecret = process.env.JWT_SECRET
 
 router.post("/v1/users", async (req, res) => {
 	const {username, password} = req.body;
@@ -32,6 +34,36 @@ router.post("/v1/users", async (req, res) => {
 		return;
 	}
 	res.status(201).json({username, games: []});
+});
+
+router.post("/v1/login", async (req, res) => {
+	const {username, password} = req.body;
+	if(typeof username !== "string" || typeof password !== "string"){
+		res.status(400).json({error: "Username and password must both be supplied and both be strings"});
+		return;
+	}
+	try{
+		const user = await (await db).collection("users").findOne({username});
+		if(user === null){
+			res.status(401).json({error: "Incorrect username or password"});
+		}
+		else if(await bcrypt.compare(password, user.passwordHash)){
+			res.status(200).json({
+				token: jwt.sign(
+					{id: user._id.valueOf()},
+					jwtSecret,
+					{expiresIn: "1d"}
+				)
+			});
+		}
+		else{
+			res.status(401).json({error: "Incorrect username or password"});
+		}
+	}
+	catch(err){
+		console.error("Error here: ", err);
+		res.status(503).json({error: "Error verifying credentials"});
+	}
 });
 
 module.exports = router;
--- test/api.js
+++ test/api.js
@@ -56,6 +56,62 @@ describe("Registration API (POST /v1/users)", () => {
 	});
 });
 
+describe("Login API (POST /v1/login)", () => {
+	describe("Malformed Requests", () => {
+		it("should return 400 if request is empty", async done => {
+			const res = await request(app).post("/api/v1/login").send({});
+			expect(res.statusCode).toBe(400);
+			expect(res.body).toHaveProperty("error");
+			done();
+		});
+		it("should return 400 if request has no password", async done => {
+			const res = await request(app).post("/api/v1/login").send({username: "testUser"});
+			expect(res.statusCode).toBe(400);
+			expect(res.body).toHaveProperty("error");
+			done();
+		});
+		it("should return 400 if request has no username", async done => {
+			const res = await request(app).post("/api/v1/login").send({password: "testPass"});
+			expect(res.statusCode).toBe(400);
+			expect(res.body).toHaveProperty("error");
+			done();
+		});
+		it("should return 400 if username is not a string", async done => {
+			const res = await request(app).post("/api/v1/login").send({username: 10, password: "testPass"});
+			expect(res.statusCode).toBe(400);
+			expect(res.body).toHaveProperty("error");
+			done();
+		});
+		it("should return 400 if password is not a string", async done => {
+			const res = await request(app).post("/api/v1/login").send({username: "testUser", password: 10});
+			expect(res.statusCode).toBe(400);
+			expect(res.body).toHaveProperty("error");
+			done();
+		});
+	});
+	describe("Valid Requests", () => {
+		it("should return 401 if username does not exist", async done => {
+			const res = await request(app).post("/api/v1/login").send({username: "notARealUser", password: "testPass"});
+			expect(res.statusCode).toBe(401);
+			expect(res.body).toHaveProperty("error");
+			done();
+		});
+		it("should return 401 if password is incorrect", async done => {
+			const res = await request(app).post("/api/v1/login").send({username: "testUser", password: "wrongPassword"});
+			expect(res.statusCode).toBe(401);
+			expect(res.body).toHaveProperty("error");
+			done();
+		});
+		it("should return 200 and a token if username and password are correct", async done => {
+			const res = await request(app).post("/api/v1/login").send({username: "testUser", password: "testPass"});
+			expect(res.statusCode).toBe(200);
+			expect(res.body).not.toHaveProperty("error");
+			expect(res.body).toHaveProperty("token");
+			done();
+		});
+	});
+});
+
 afterAll(done => {
 	require("../api/db-utils").client.close();
 	done()
