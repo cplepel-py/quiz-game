46f48b57da4737f09cf79ee44db844357c1c2bec
unknown <cbplepel@gmail.com>
2020-08-09 13:51:41 -0500

    add type verification when creating new games

--- api/games.js
+++ api/games.js
@@ -3,24 +3,60 @@ const {db} = require("./db-utils.js");
 const {verifyToken} = require("./auth.js");
 const router = require("express").Router();
 
+function verifyBoard(board){
+	let length = undefined;
+	for(let [k, cat] in Object.entries(board)){
+		if(typeof k !== "string")
+			throw new TypeError("All board keys (category names) must be strings");
+		if(!Array.isArray(cat))
+			throw new TypeError("Each category must be an array of questions");
+		if(length === undefined)
+			length = cat.length
+		else if(length !== cat.length)
+			throw new TypeError("Each category must have the same number of questions");
+		if(cat.some(q => 
+			typeof q.question !== "string" || typeof q.answer !== "string"
+		)) throw new TypeError("Each question have string question and answers");
+	}
+	return length;
+}
+
+function parseGame({
+		title,
+		editors=[],
+		isPrivate=false,
+		tags=[],
+		interval=100,
+		board={}
+	}={}){
+	if(typeof title !== "string")
+		throw new TypeError("title must be a string and is required");
+	if(!Array.isArray(editors) ||
+	   !editors.every(e => (/^[a-zA-Z0-9]{24}$/.test(e))))
+		throw new TypeError("editors must be an array of user ids");
+	if(typeof isPrivate !== "boolean")
+		throw new TypeError("isPrivate must be a boolean");
+	if(!Array.isArray(tags) || !tags.every(e => typeof e === "string"))
+		throw new TypeError("tags must be an array of strings");
+	if(typeof interval !== "number")
+		throw new TypeError("interval must be a number");
+	if(typeof board !== "object" || board === null)
+		throw new TypeError("board must be a non-null object");
+	verifyBoard(board);
+	return {title, editors, isPrivate, tags, interval, board};
+}
+
 router.post("/v1/games", async (req, res) => {
 	try{
 		const {auth, claims} = await verifyToken(req.get("x-access-token"), res);
 		if(auth){
-			const {title, editors, isPrivate, tags, interval, board} = req.body;
-			if(typeof title !== "string")
-				return res.status(400).json({error: "title must be a string"})
-			const {insertedId} = await (await db).collection("games").insertOne({
-				title,
-				editors: (editors || []).push(claims.id),
-				isPrivate: isPrivate || false,
-				tags: tags || [],
-				interval: interval || 100,
-				board: board || {}
-			});
+			const game = parseGame(req.body);
+			game.editors.push(claims.id);
+			const {insertedId} = await (await db).collection("games")
+				.insertOne(game);
 			await (await db).collection("users").updateOne(
 				{_id: ObjectId(claims.id)},
-				{$push: {games: insertedId}}
+				{$addToSet: {games: insertedId}}
 			);
 			res.status(200).json({id: insertedId.valueOf()});
 		}
@@ -28,8 +64,10 @@ router.post("/v1/games", async (req, res) => {
 	catch(err){
 		if(err instanceof MongoError && err.code == 121)
 			res.status(400).json({error: "Invalid data"});
+		else if(err instanceof TypeError){
+			res.status(400).json({error: err.message});
+		}
 		else{
-			console.error("Error here: ", err);
 			res.status(503).json({error: "Error creating game"});
 		}
 	}
