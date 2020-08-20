3c2ca6e04cb43c85e87b46881fd25423fa34c318
unknown <cbplepel@gmail.com>
2020-08-20 11:47:28 -0500

    add marko setup

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ if(process.env.NODE_ENV !== "production"){
 const express = require("express");
 const app = express();
 const apiRouter = require("./api/api");
+const {serveStatic} = require("lasso/middleware");
+require("marko/node-require").install();
+
+require("lasso").configure({
+	plugins: ["lasso-marko"]
+});
+
 app.use("/api", apiRouter);
+
+app.use(serveStatic());
 
 if(process.env.NODE_ENV !== "test"){
 	app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
