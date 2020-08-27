5188f9fc19b31f2873ea3d489e06042f4203b41a
unknown <cbplepel@gmail.com>
2020-08-27 10:57:21 -0500

    remove Nexmo 2FA functions

--- api/auth.js
+++ api/auth.js
@@ -5,14 +5,6 @@ const speakeasy = require("speakeasy");
 
 const jwtSecret = process.env.JWT_SECRET;
 
-class SMSError extends Error{};
-class AuthenticationError extends Error{};
-
-const nexmo = new (require("nexmo"))({
-	apiKey: process.env.NEXMO_API_KEY,
-	apiSecret: process.env.NEXMO_API_SECRET
-});
-
 async function set2FA(user){
 	const query = typeof user === "string" ? {username: user} : {_id: user};
 	const otpauth = speakeasy.generateSecret({name: "Quiz Game"}).base32;
@@ -40,54 +32,6 @@ async function check2FA(user, code){
 	});
 	if(valid) return {auth: true};
 	return {auth: false, error: "Incorrect code"};
-}
-
-async function sendSMS(user, rel="password", number=undefined){
-	const query = typeof user === "string" ? {username: user} : {_id: user};
-	const tel = number || (await (await db).collection("users").findOne(query))
-		.number;
-	return await new Promise((resolve, reject) => {
-		nexmo.verify.request({
-			number: tel,
-			brand: "QuizGame",
-			length: 6,
-			workflow_id: 6
-		}, async (err, result) => {
-			if(err) reject(err);
-			else if(result.status == "0"){
-				db.then(conn => {
-					conn.collection("users").updateOne(
-						query,
-						{$set: {[`request_id.${rel}`]: result.request_id}}
-					).then(() => resolve(result.request_id)).catch(reject);
-				}).catch(reject);
-			}
-			else reject(new SMSError(`Nexmo error ${result.status}`));
-		});
-	});
-}
-
-async function authenticateCode(user, code, rel="password"){
-	const query = typeof user === "string" ? {username: user} : {_id: user};
-	const request_id = (await (await db).collection("users").findOne(query))
-		.request_id[rel];
-	if(!request_id) throw new AuthenticationError("No code was requested");
-	return await new Promise((resolve, reject) => {
-		nexmo.verify.check({request_id, code}, (err, result) => {
-			if(err) reject(err);
-			else if(result.status == "0"){
-				db.then(conn => {
-					conn.collection("users").updateOne(
-						query,
-						{$set: {[`request_id.${rel}`]: undefined}}
-					).then(() => resolve(request_id)).catch(reject);
-				}).catch(reject);
-			}
-			else reject(new AuthenticationError(
-				`Nexmo error ${result.status}: ${result.error_text}`
-			));
-		});
-	});
 }
 
 async function verifyToken(token, res, {ids=null, message="Unauthorized"}={}){
@@ -120,6 +64,5 @@ async function verifyToken(token, res, {ids=null, message="Unauthorized"}={}){
 }
 
 module.exports = {
-	SMSError, AuthenticationError, sendSMS, authenticateCode, verifyToken,
-	set2FA, unset2FA, check2FA
+	verifyToken, set2FA, unset2FA, check2FA
 };
